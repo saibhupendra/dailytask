4c44daf7378fd2e7ffe55e928a10c29d6b02a671
saibhupendra <saibhupendra21@gmail.com>
2024-10-07 19:24:08 +0530

    Add files via upload

--- 10.py
+++ 10.py
@@ -1,12 +1,5 @@
-"write a program toi check if a number is an armstrong"
-n =int(input("enter a number:"))
-num=n
-s=0
-while n>0:
-    r=n%10
-    s=s+r*r*r
-    n=n//10
-if s==num:
-    print(num," Amstrong number")
-else:
-    print(num, "Not an Amstrong")
+##Python program to concatenate two strings using join() method. 
+s="python"
+s1="learning"
+print(s+s1)
+print("".join([s,s1]))
