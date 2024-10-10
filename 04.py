721cb1f1b8e9e825e8fd11ac6f88d4eddaf07e8b
saibhupendra <saibhupendra21@gmail.com>
2024-10-10 19:21:53 +0530

    Add files via upload

--- 04.py
+++ 04.py
@@ -1,10 +1,28 @@
-##Python program to replace the string space with a given character. 
-s = "python learning"  
-c = " is easy to "  
-s1 = ""  
-for i in s:
-    if i == " ":
-        s1 += c 
-    else:
-        s1 += i 
-print(" string:", s1)
+###Sorting and Reversing Lists:
+##Write a Python program to sort a list of 10 random integers in ascending and 
+# descending order using sort() and reverse().
+##Create a list of strings and reverse the order of elements using both reverse() and 
+# slicing [::-1]. Compare the results.
+
+
+l=[1,3,4,2,6,9,1,0,11,44,66]
+l.sort()
+print(l)
+l.reverse()
+print(l)
+
+##Create a list of strings and reverse the order of elements using both reverse() and 
+# slicing [::-1]. Compare the results.
+l=["nani","nagu","prasad","ramadevi"]
+print(l)
+l1=l
+l1.reverse()
+print(l1)
+l2=l
+l2[::-1]
+print(l2)
+if (l1==l2):
+    print("true")
+else:
+    print("flase")    
+
