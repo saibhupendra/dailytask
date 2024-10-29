78bcd67d6fc4ea3e681a5c23f1ae3b4f8798841f
saibhupendra <saibhupendra21@gmail.com>
2024-10-29 17:53:01 +0530

    add all

---
+++ 25-10-2024.py
@@ -0,0 +1,24 @@
+## packages :
+##      >> it is a collections of modules
+##modules  >>collection of functions,variable,class
+#function>>reusable blick of code
+
+
+from packages.m1 import add
+
+a=10
+b=9
+print(add(a,b))
+
+
+from packages.m2 import mul
+
+x=20
+y=2
+print(mul(x,y))
+
+
+
+## we can access the packages into into ur programming
+## we cannot access the packages to another package
+
